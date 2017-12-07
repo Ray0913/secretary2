df5d1a0d8fa20cbfff9d0f7fe10b2a56cce4112b
zxcvc851203 <zxcvc851203@yahoo.com.tw>
2017-12-06 21:27:49 -0500

    first commit

---
+++ mysite3/question/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    publication_date = models.DateTimeField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    content = models.TextField()
